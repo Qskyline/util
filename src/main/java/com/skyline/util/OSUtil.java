de0e8728a8df9ada68dcf6c5159ed33f740a1a13
skyline <1002485975@qq.com>
2018-01-12 15:57:46 +0800

    add execShell feature.

--- src/main/java/com/skyline/util/OSUtil.java
+++ src/main/java/com/skyline/util/OSUtil.java
@@ -1,6 +1,11 @@
 package com.skyline.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -9,6 +14,10 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
 
 public class OSUtil {
 	public static List<String> getLanIp() throws SocketException, UnknownHostException{
@@ -47,11 +56,47 @@ public class OSUtil {
 		return ((double)diskPartition.getFreeSpace())/1024/1024/1024;
 	}
 	
-	public static void main(String[] args) {
+	public static String execShell(String ip, String user, String password, String shell) {
+		String  DEFAULTCHART="UTF-8";
+		String result = null;
 		try {
-			System.out.println(getLanIp());
-		} catch (SocketException | UnknownHostException e) {
+			Connection conn = new Connection(ip); 
+            conn.connect();
+            if(conn.authenticateWithPassword(user, password)) {
+            	Session session = conn.openSession();
+				session.execCommand(shell);
+				result = processStdout(session.getStdout(), DEFAULTCHART);
+				if(StringUtils.isBlank(result)){
+					result=processStdout(session.getStderr(), DEFAULTCHART);  
+		        }
+				session.close(); 
+            }
+            conn.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		return result;		
+	}
+	
+	private static String processStdout(InputStream in, String charset) {
+		InputStream stdout = new StreamGobbler(in);
+		StringBuffer buffer = new StringBuffer();
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(stdout, charset));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				buffer.append(line + "\n");
+			}
+			br.close();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return buffer.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(execShell("54.222.241.235", "awsskyline", "Eas@2016", "pwd"));
 	}
 }
