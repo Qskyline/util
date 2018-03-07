e83792e5f60d1d8256cda81789e43e428c9aafe7
skyline <1002485975@qq.com>
2018-03-07 12:38:55 +0800

    code format.

--- src/main/java/com/skyline/util/MapRemoveNullUtil.java
+++ src/main/java/com/skyline/util/MapRemoveNullUtil.java
@@ -9,27 +9,27 @@ import java.util.Set;
 import org.apache.commons.lang.StringUtils;
 
 public class MapRemoveNullUtil {
-    public static void removeNullEntry(Map<?, ?> map){  
-        removeNullKey(map);  
-        removeNullValue(map);  
-    }  
+    public static void removeNullEntry(Map<?, ?> map){
+        removeNullKey(map);
+        removeNullValue(map);
+    }
 
     public static void removeNullKey(Map<?, ?> map){
         Set<?> set = map.keySet();
-        for (Iterator<?> iterator = set.iterator(); iterator.hasNext();) {  
-            Object obj = (Object)iterator.next();  
-            remove(obj, iterator);  
-        }  
+        for (Iterator<?> iterator = set.iterator(); iterator.hasNext();) {
+            Object obj = iterator.next();
+            remove(obj, iterator);
+        }
     }  
 
-    public static void removeNullValue(Map<?, ?> map){  
-        Set<?> set = map.keySet();
+    public static void removeNullValue(Map<?, ?> map){
+        Set set = map.keySet();
         for (Iterator<?> iterator = set.iterator(); iterator.hasNext();) {
-            Object obj = (Object)iterator.next();  
-            Object value =(Object)map.get(obj);  
+            Object obj = iterator.next();
+            Object value = map.get(obj);
             remove(value, iterator);
-        }  
-    }  
+        }
+    }
 
     private static void remove(Object obj,Iterator<?> iterator){
     	if(obj == null){
@@ -51,27 +51,27 @@ public class MapRemoveNullUtil {
             }              
         } else if(obj instanceof Object[]){  
             Object[] array =(Object[])obj;
-            if(array.length <= 0){  
-                iterator.remove();  
+            if(array.length <= 0){
+                iterator.remove();
             }
         }
     }
     
     public static void main(String[] args) { 
-        Map<Object, String> map = new HashMap<Object, String>();
+        Map<Object, String> map = new HashMap<>();
         map.put(1, "第1个值是数字");  
         map.put("2", "第2个值是字符串");  
         map.put(new String[]{"1","2"},"第3个值是数组");  
-        map.put(new ArrayList<Object>(), "第4个值是List");
-        map.put(new HashMap<Object, Object>(), "Map 无值");
-        map.put("5", "第5个");  
-        map.put("6",null);  
+        map.put(new ArrayList<>(), "第4个值是List");
+        map.put(new HashMap<>(), "Map 无值");
+        map.put("5", "第5个");
+        map.put("6",null);
         map.put("7", "");
         map.put("8", "  ");
-        System.out.println(map);  
-        MapRemoveNullUtil.removeNullKey(map); 
-        System.out.println(map);  
-        MapRemoveNullUtil.removeNullValue(map);  
-        System.out.println(map);  
+        System.out.println(map);
+        MapRemoveNullUtil.removeNullKey(map);
+        System.out.println(map);
+        MapRemoveNullUtil.removeNullValue(map);
+        System.out.println(map);
     }
 }
